715b87f6ee8c539205398d205ba34d52060bda69
Kostad1n <71552347+KostadinKim@users.noreply.github.com>
2022-02-18 20:15:03 +0600

    Add files via upload

---
+++ RootWebDev/Lab3/JSINFO/var1.js
@@ -0,0 +1,26 @@
+/* 1 */
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin);
+
+/* 2*/
+let ourPlanetName = "Earth";
+let CurrentUserName = "Kostadin";
+
+
+
+/* 3 */
+const birthday = '18.04.1982';
+
+const age = someCode(birthday);
+
+const BIRTHDAY = '18.04.1982'; // использовать заглавные буквы?
+
+const AGE = someCode(BIRTHDAY); // а здесь?
+
+// переменная age вычисляется во время работы скрипта, поэтому она менее константна чем 'const BIRTHDAY'
+// и стоит писать их маленькими буквами
+
+
